7c95beeae166f032fd806e8b57dead923abef3a0
Nin <nin98683@gmail.com>
2025-07-03 10:24:46 +0700

    fix bug

--- android/src/main/kotlin/com/ayeee/blue_print_pos/Logger.kt
+++ android/src/main/kotlin/com/ayeee/blue_print_pos/Logger.kt
@@ -3,8 +3,10 @@ package com.ayeee.blue_print_pos
 import android.util.Log
 
 object Logger {
+    private const val isDebug = false 
+
     fun log(message: String) {
-        if (BuildConfig.DEBUG) {
+        if (isDebug) {
             Log.d("BluePrintPos", message)
         }
     }
